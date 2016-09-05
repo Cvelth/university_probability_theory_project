fabbbdcc06e52685fa97a55dec083260bb94c692
Vania <Vanya.Krisak@gmail.com>
2016-09-05 16:59:10 +0300

    Polinomial v0.2

--- Lab1/Polynomial.h
+++ Lab1/Polynomial.h
@@ -4,16 +4,25 @@
 class Polynomial
 {
 	std::set<size_t> ones;
+	size_t size;
 
 public:
 
-	Polynomial() {	}
+	Polynomial() : size(0) {	}
 
-	Polynomial(std::initializer_list<size_t> ones) : ones(ones)	{	}
+	Polynomial(std::initializer_list<size_t> data) : size(0)
+	{	
+		for each (auto var in data)
+		{
+			ones.insert(var);
+			size = var > size ? var : size;
+		}
+	}
 
 	//test func
 	Polynomial* generate(size_t size, size_t capacity = 2)
 	{
+		this->size = size;
 		ones.insert(0);
 		for (int i = 1; i < size & ones.size() < capacity; i++)
 			if (size % i != 0)
